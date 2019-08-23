67bb870c4dc4b1a75a0b2cd19247c52b35492a49
Felipe <felipe@soulmkt.com>
2019-08-23 16:28:31 -0300

    first commit

---
+++ install_docker.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+sudo apt-get remove docker.io docker-engine
+
+sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
+
+curl -fsSL https://mirrors.ustc.edu.cn/docker-ce/linux/debian/gpg | sudo apt-key add -
+
+sudo apt-key fingerprint 0EBFCD88
+
+sudo apt-get update
+
+sudo apt-get install docker-ce
+
+systemctl start docker
+
+sudo usermod -aG docker $USER
+
+sudo mkdir -p /etc/docker
+sudo tee /etc/docker/daemon.json <<-'EOF'
+{
+  "registry-mirrors": ["https://jxus37ad.mirror.aliyuncs.com"]
+}
+EOF
+sudo systemctl daemon-reload
+sudo systemctl restart docker
