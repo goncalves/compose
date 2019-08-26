1f5d233cac7f0567cf34eb606cff3dbe0e412cb1
Felipe <felipe@soulmkt.com>
2019-08-26 11:53:53 -0300

    ajuste diretorio

--- install_docker.sh
+++ install_docker.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 sudo apt-get remove docker.io docker-engine
 
+sudo apt-get remove --auto-remove docker
+sudo apt-get purge docker-ce
+sudo rm -rf /var/lib/docker 
+
 sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
 
 curl -fsSL https://mirrors.ustc.edu.cn/docker-ce/linux/debian/gpg | sudo apt-key add -
